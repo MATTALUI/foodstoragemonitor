777ef67c187235dd35a20819b16f88a333658029
Matthew Hummer <biggreyson@gmail.com>
2022-04-07 10:51:10 -0600

    add commands utils; pimp has a grin

---
+++ commands.sh
@@ -0,0 +1,4 @@
+export FLASK_ENV=development # These commands are mostly used in production but this means it will live reload changes
+alias start-https='sudo flask run --host=0.0.0.0 --port=443 --cert=adhoc'
+alias start-http='sudo flask run --host=0.0.0.0 --port=80'
+alias kill-foodmon='pgrep -f flask | xargs sudo kill -9'
